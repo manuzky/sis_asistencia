65f4e22e88fb7050ef27d21aea5537df45795b5c
Zakeyo <manuelc.dev@gmail.com>
2024-12-15 14:48:49 -0400

    Arreglo del config:cache

--- config/app.php
+++ config/app.php
@@ -184,7 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        $app->register(\Barryvdh\DomPDF\ServiceProvider::class),
+        // $app->register(\Barryvdh\DomPDF\ServiceProvider::class),
     ])->toArray(),
 
 
